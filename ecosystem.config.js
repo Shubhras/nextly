1f3ecb5b4a62a97119582dc17935f2fc83ee0a22
shubhra <jshubhra@digiprima.com>
2022-06-02 19:30:14 +0530

    chnages port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,18 +5,21 @@ module.exports = {
             cwd: './website/',
             script: 'npm',
             args: 'start',
+            port:'3001',
         },
         {
             name: 'admin',
             cwd: './admin/',
             script: 'npm',
             args: 'start',
+            port:'8000',
         },
         {
             name: 'backend',
             cwd: './backend/',
             script: 'nodemon',
             args: 'server.js',
+            port:'4002',
         },
         // optionally a second project
     ],
